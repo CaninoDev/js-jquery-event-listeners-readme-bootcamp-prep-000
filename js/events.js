507e80473afb611da8653847c11e6f1b5c2dc0ec
Ian Canino <ian.canino@gmail.com>
2018-03-24 16:08:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,7 +23,9 @@ function frameIt () {
 
 function pressIt () {
   $(document).on('keydown', function(key) {
-    if(key.which == )
+    if(key.which == 71) {
+      alert("You have pressed the G key")
+    }
   })
 }
 
