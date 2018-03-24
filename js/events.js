97877512d545e58573128415ac2e8484d41af2f8
Ian Canino <ian.canino@gmail.com>
2018-03-24 15:49:38 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,17 @@
 
 $(document).ready(function(){
 
-// call functions here
+$('h1').on("eventname", function(){
+    //action you want taken
+});
 
+function getIt () {
+  $('p').on("click", funtion (){
+    alert("Hey!")
+  })
+}
+
+functiom frameIt (){
+  
+}
 });
